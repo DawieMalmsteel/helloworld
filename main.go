f55677994ba1d89bfd05d1ce140a0f0e492fbd0f
dwcks <corclan19@gmail.com>
2025-12-18 16:04:54 +0700

    learn struct

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	concur()
+	// concur()
+
+	mapPersons := map[string]string{
+		"Alice":      "30",
+		"Bob":        "25",
+		"Borderland": "40",
+	}
+
+	for i, p := range mapPersons {
+		fmt.Println(i, p)
+	}
 }
